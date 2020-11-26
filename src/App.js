7a4de34fd2a0b2c5fc72087be3967adb0d027e0b
Lauren Dove <elledoveinc@gmail.com>
2020-11-26 11:37:54 -0500

    installed fontawesome solid version.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Song from './Components/Song';
+import Player from './Components/Player';
 
 function App() {
   return (
     <div className="App">
-      <h1> Elles Music Player</h1>
+      <h1> Welcome To Elles Music Player</h1>
+      <Song/>
+      <Player/>
+
     </div>
   );
 }
--- src/Components/Song.js
+++ src/Components/Song.js
@@ -3,8 +3,11 @@ import React from 'react';
 
 const Song = () => {
     return (
-        <div className="song">
-            <h1> Player</h1>
+        <div className="song-container">
+            <h1> Picture</h1>
+            <h1>Song name</h1>
+            <h1>Artist</h1>
+
 
         </div>
     )
