eef7f1138b8112647e1233f5f80532e300e504ad
Lauren Dove <elledoveinc@gmail.com>
2021-01-19 17:01:34 -0500

    imported useState to App component. State will contain song data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React,{useState} from 'react';
 //importing styles from SAAS
 import "./styles/app.scss";
 import Song from './Components/Song';
 import Player from './Components/Player';
+import data from './util';
 
 function App() {
+  //This will return to me all the songs from the util file.
+  const [songs,setSongs]=useState(data())
   return (
     <div className="App">
       <h1 className="welcome"> Welcome To Elles Music Player</h1>
