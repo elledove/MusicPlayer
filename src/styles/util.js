f5f2e30daa5cea16e5703c346700ca188a3859b0
Lauren Dove <elledoveinc@gmail.com>
2021-01-19 15:56:48 -0500

    created util.js file to house music playlist

---
+++ src/styles/util.js
@@ -0,0 +1,22 @@
+import {v4 as uuid4} from "uuid";
+
+function chillTrax(){
+    return [
+
+        {name: "Blue Stripes",
+        cover: "https://chillhop.com/wp-content/uploads/2020/05/260a909fca701bbb7593f5f8f9cff9508cca2856-1024x1024.jpg",
+    artist: "j'san, Epektase",
+    audio: "",
+color:"",
+id:"",
+}
+
+
+
+
+
+    ]
+
+}
+
+export default chillTrax;
