d588003ddb6a5cc348ec15605958bc3172007ada
Lauren Dove <elledoveinc@gmail.com>
2021-03-02 15:41:26 -0500

    added style to .library to take up left side of screen by 20rem.holds all songs.y

--- src/Components/Library.js
+++ src/Components/Library.js
@@ -3,7 +3,7 @@ import LibrarySong from './LibrarySong';
 
 
 const Library = (props) => {
-    return (  <div className="Library">
+    return (  <div className="library">
         <h2>Library of Jams</h2>
         <div className="library-songs">
        { props.songs.map((song) =>  <LibrarySong song={song} />   )}
