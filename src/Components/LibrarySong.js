7d1d20cca8f1108cec075f86234c83c4abc9aeed
Lauren Dove <elledoveinc@gmail.com>
2021-03-03 21:20:27 -0500

    created new div for h3 & h4 tags in library song component.

--- src/Components/LibrarySong.js
+++ src/Components/LibrarySong.js
@@ -4,8 +4,10 @@ import React from 'react';
 const LibrarySong = (props) => {
     return ( <div className="library-song">
       <img alt={props.song.name} src={props.song.cover}></img>
+              <div className="song-description">
             <h3>{props.song.name}</h3>
             <h4>{props.song.artist}</h4>
+            </div>
 
 
     </div> );
